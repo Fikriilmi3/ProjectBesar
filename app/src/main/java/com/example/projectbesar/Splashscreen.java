c5eea6208e6791c481dfe58b53a599fbde698949
Fikriatul Ilmi <fikriyatulilmi@gmail.com>
2023-03-29 10:57:38 +0700

    Splash screen

---
+++ app/src/main/java/com/example/projectbesar/Splashscreen.java
@@ -0,0 +1,28 @@
+package com.example.projectbesar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends AppCompatActivity {
+
+    public static int SPLASH_TIMER = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashscreen.this, NavigationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+}
