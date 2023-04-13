758bda3579587f70dc28f9e5bbfc6e8fe3bfb013
Fikriatul Ilmi <fikriyatulilmi@gmail.com>
2023-04-13 12:58:00 +0700

    Bottom Bar

--- app/src/main/java/com/example/projectbesar/LoginActivity.java
+++ app/src/main/java/com/example/projectbesar/LoginActivity.java
@@ -2,13 +2,41 @@ package com.example.projectbesar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btn_login;
+    TextView tvreg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_login = findViewById(R.id.btn_login);
+        tvreg = findViewById(R.id.tvreg);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        tvreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/projectbesar/RegisterActivity.java
+++ app/src/main/java/com/example/projectbesar/RegisterActivity.java
@@ -2,13 +2,43 @@ package com.example.projectbesar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button btn_register;
+    TextView tvlogin;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btn_register = findViewById(R.id.btn_register);
+        tvlogin = findViewById(R.id.tvlogin) ;
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        tvlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
