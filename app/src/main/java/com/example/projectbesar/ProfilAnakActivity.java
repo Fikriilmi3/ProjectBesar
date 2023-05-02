3b8b03a911784327a98fd8c3bae40995ee3edcea
Fikriatul Ilmi <fikriyatulilmi@gmail.com>
2023-05-02 15:18:41 +0700

    profile

---
+++ app/src/main/java/com/example/projectbesar/ProfilAnakActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectbesar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilAnakActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_anak);
+    }
+}
